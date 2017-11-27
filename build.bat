fca3a05d5fa9065b8d01a6cac911ed88d197e8eb
Keith Ott <KeithOT@buyseasons.com>
2017-11-27 11:43:17 -0600

    Fixed pathing in compressed ZIP

--- build.bat
+++ build.bat
@@ -1,5 +1,11 @@
 del PlaylistBuilder\bin\Release\PlaylistBuilder-*.zip
 rmdir /q /s PlaylistBuilder\bin\Release\Publish
 dotnet publish --output bin\Release\Publish --configuration Release --self-contained --runtime win-x64
-7za a PlaylistBuilder\bin\Release\PlaylistBuilder-%date:~10,4%%date:~4,2%%date:~7,2%.zip PlaylistBuilder\bin\Release\Publish\*.* -mx9
+
+cd PlaylistBuilder\bin\Release\Publish
+
+..\..\..\..\7za.exe a ..\PlaylistBuilder-%date:~10,4%%date:~4,2%%date:~7,2%.zip *.* -mx9
+
+cd ..\..\..\..\
+
 explorer PlaylistBuilder\bin\Release\
